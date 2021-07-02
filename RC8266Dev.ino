281260cd00eff6164e397e69195e770abc35a49d
TrevM <trevmosele@gmail.com>
2021-07-02 16:34:28 +0100

    Add file header

--- RC8266Dev.ino
+++ RC8266Dev.ino
@@ -1,4 +1,7 @@
 /*
+ * Use ESP8266 as RC, this is the Remote Controlled Device
+ * It is WiFi server, listening for UDP messages from Controller (client) with H & V percentages
+ * Also responds with UDP containing current Battery Voltage
 */
 #include "servos.h"
 #include "debug.h"
